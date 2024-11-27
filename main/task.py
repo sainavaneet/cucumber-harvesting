769acd287e6802010af706a7e61a85e2649250fe
Navaneet <sainavaneet76@gmail.com>
2024-11-27 19:00:31 +0900

    u

--- main/task.py
+++ main/task.py
@@ -8,30 +8,30 @@ class CucumberHarvesting:
 
     def perform_harvest(self):
        
-        # self.bot.arm.go_to_home_pose()
+        self.bot.arm.go_to_home_pose()
         self.bot.arm.set_ee_pose_components(x=0.4, z=0.3)
-        # self.bot.arm.set_single_joint_position("waist", np.pi / 2.0)
-        # self.bot.gripper.open()
+        self.bot.arm.set_single_joint_position("waist", np.pi / 2.0)
+        self.bot.gripper.open()
 
-        # # Move to harvesting position and close gripper
-        # self.bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-        # self.bot.gripper.close()
+        # Move to harvesting position and close gripper
+        self.bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
+        self.bot.gripper.close()
 
-        # # Retract and reset waist
-        # self.bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-        # self.bot.arm.set_single_joint_position("waist", -np.pi / 2.0)
+        # Retract and reset waist
+        self.bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+        self.bot.arm.set_single_joint_position("waist", -np.pi / 2.0)
 
-        # # Adjust pitch and reset waist again
-        # self.bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
-        # self.bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
-        # self.bot.arm.set_single_joint_position("waist", np.pi / 2.0)
+        # Adjust pitch and reset waist again
+        self.bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
+        self.bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
+        self.bot.arm.set_single_joint_position("waist", np.pi / 2.0)
 
-        # # Open gripper and return to home pose
-        # self.bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-        # self.bot.gripper.open()
-        # self.bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-        # self.bot.arm.go_to_home_pose()
-        # self.bot.arm.go_to_sleep_pose()
+        # Open gripper and return to home pose
+        self.bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
+        self.bot.gripper.open()
+        self.bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+        self.bot.arm.go_to_home_pose()
+        self.bot.arm.go_to_sleep_pose() 
 
 if __name__ == '__main__':
     # Create an instance of the CucumberHarvesting class and perform the harvest
--- main/transform.py
+++ main/transform.py
@@ -2,16 +2,14 @@ import tf
 import rospy
 
 def get_transformation(target_frame, source_frame):
+    rospy.init_node('tf_listener111', anonymous=True) 
     listener = tf.TransformListener()
-    rospy.init_node('tf_listener')
 
-    # Wait for up to 10 seconds for the frame to become available
-    try:
+
+    while True:
         listener.waitForTransform(target_frame, source_frame, rospy.Time(), rospy.Duration(10.0))
         (trans, rot) = listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
         print(f"Translation: {trans}, Rotation: {rot}")
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-        print(f"Error: {e}")
 
-# Example usage
-get_transformation('/puppet_left/gripper_link', '/world')  # Adjust source_frame as necessary
+
+get_transformation('/puppet_left/gripper_link', '/world')  
